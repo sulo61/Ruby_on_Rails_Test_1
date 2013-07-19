67c9b9c44a2f731d4d5b17def8fd0799c095b0ad
sulo61 <sulo612@gmail.com>
2013-07-19 03:58:43 -0700

    zmiana widoku, polaczenie panel i results

--- app/controllers/stat_controller.rb
+++ app/controllers/stat_controller.rb
@@ -1,30 +1,22 @@
 class StatController < ApplicationController
 
-  $searchType = 1
   $daysBack = 30
  
   def panel
-	
+	@activities = Activity.showTable(Date.today-$daysBack)
 	if params[:all]		
-		$searchType = 1
-		redirect_to :action => "results"
+		@activities = Activity.showTable()
 	end
 	if params[:last]			
-		$searchType = 2
-		$daysBack = params[:numberOfDays].to_i
-		redirect_to :action => "results"
+		@activities = Activity.showTable(Date.today-params[:numberOfDays].to_i)
+		
 		
 	end
 	
   end
 
   def results
-	if $searchType == 1
-		@activities = Activity.showTable()
-	end
-	if $searchType == 2
-		@activities = Activity.showTable(Date.today-$daysBack)
-	end
+	
   end
 
   def details
