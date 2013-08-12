43a8b0289f3b49bc1676cb2f1ed77bc36a91c382
sulo61 <sulo612@gmail.com>
2013-08-12 06:08:12 -0700

    clients, procent aktywnych

--- app/models/event_log.rb
+++ app/models/event_log.rb
@@ -24,19 +24,53 @@ class EventLog
 		idArray = idArray.flatten
 		eventSum = 0
 		activeUsrs = 0
+		aEvent = 0
+		wEvent = 0
+		iEvent = 0
+		uEvent = 0
+		client = "-"
 
 		idArray.each do |ida|
-			tmp = (EventLog.where(:created_at => { '$gte' => actDateFrom, '$lte' => actDateTo } ).where("user_id" => Moped::BSON::ObjectId(ida)).count)
-			eventSum += tmp
-			activeUsrs += 1 if tmp>0
+			events = (EventLog.where(:created_at => { '$gte' => actDateFrom, '$lte' => actDateTo } ).where("user_id" => Moped::BSON::ObjectId(ida)))
+			
+			
+			eventsCount = events.size
+			eventSum += eventsCount
+			if ( eventsCount>0 )
+				activeUsrs += 1
+				events.each do |fc|
+					client = fc.to_a[0][:client].to_s
+					case client
+						when "web"
+							wEvent += 1
+			
+						when "Android"
+							aEvent += 1
+			
+						when "ios"
+							iEvent += 1
+			
+						when "unknown"
+							uEvent += 1
+			
+					end		
+				end
+			end
+		
 		end
-
+		count = (u["value"]["Created"]).to_i
+		percentActiveUsrs = (((activeUsrs.to_f / count.to_f) * 100 ).to_s )[0,5]+" %"
 		{ 
 			:date => u["_id"]["data"],
-			:count => (u["value"]["Created"]).to_i,
+			:count =>  count,
 			:id => idArray,
 			:eventSum => eventSum,
-			:activeUsrs => activeUsrs
+			:androidEvent => aEvent,
+			:webEvent => wEvent,
+			:iosEvent => iEvent,
+			:unknownEvent => uEvent,
+			:activeUsrs => activeUsrs,
+			:percentActiveUsrs => percentActiveUsrs
 		}		
 	end
   end
